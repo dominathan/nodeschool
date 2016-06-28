4cf9ed90c3bc4f2438d66857ee76525f98be650a
Nathan Hall <nathan.mh@gmail.com>
2016-06-28 12:10:24 -0400

    finish learnyounode

--- learnyounode/learnyounode.js
+++ learnyounode/learnyounode.js
@@ -66,16 +66,109 @@
 
 /* ------------------------------------- */
 
+// var http = require('http');
+// var bl = require('bl');
+//
+// http.get(process.argv[2],function(resp) {
+//
+//   resp.pipe(bl(function(err,data) {
+//     console.log(data.toString().length);
+//   }))
+//
+//   resp.pipe(bl(function(err,data) {
+//     console.log(data.toString());
+//   }))
+// });
+
+/* ------------------------------------- */
+
+// var http = require('http');
+// var bl = require('bl');
+//
+// function getHttpData(url) {
+//   return new Promise(function(resolve,reject) {
+//     http.get(url,function(resp) {
+//       resp.pipe(bl(function(err,data) {
+//         resolve(data.toString())
+//       }))
+//     });
+//   })
+// }
+//
+// var request1 = getHttpData(process.argv[2])
+// var request2 = getHttpData(process.argv[3])
+// var request3 = getHttpData(process.argv[4])
+//
+// Promise.all([request1,request2,request3])
+// .then(function(data) {
+//   data.forEach(function(line) {
+//     console.log(line);
+//   })
+// })
+
+/* ------------------------------------- */
+
+// var net = require('net');
+// var server = net.createServer(function (socket) {
+//
+//    var date = new Date(Date.now() - 1000*60*60*4).toISOString().slice(0,16).replace('T',' ') + "\n";
+//    socket.end(date);
+// })
+// server.listen(process.argv[2]);
+
+/* ------------------------------------- */
+
+// var fs = require('fs');
+// var http = require('http');
+//
+// var server = http.createServer(function(req,res) {
+//   var readStream = fs.createReadStream(process.argv[3])
+//
+//   readStream.on('open', function() {
+//     readStream.pipe(res);
+//   })
+// })
+// server.listen(process.argv[2]);
+
+/* ------------------------------------- */
+
+// var fs = require('fs');
+// var http = require('http');
+// var map2 = require('through2-map');
+//
+// var server = http.createServer(function(req,res) {
+//   if(req.method === "POST") {
+//     req.pipe(map2(function(chunk) {
+//       return chunk.toString().toUpperCase();
+//     })).pipe(res);
+//   }
+// })
+// server.listen(process.argv[2]);
+
+/* ------------------------------------- */
+
 var http = require('http');
-var bl = require('bl');
+var url = require('url');
 
-http.get(process.argv[2],function(resp) {
+var server = http.createServer(function(req,res) {
+  res.writeHead(200,{'Content-Type': 'application/json'})
 
-  resp.pipe(bl(function(err,data) {
-    console.log(data.toString().length);
-  }))
+  var urlReq = url.parse(req.url,true)
+  var date = new Date(urlReq.query.iso);
 
-  resp.pipe(bl(function(err,data) {
-    console.log(data.toString());
-  }))
-});
+  if(urlReq.pathname === '/api/parsetime') {
+    res.end(JSON.stringify({
+      hour: date.getHours(),
+      minute: date.getMinutes(),
+      second: date.getSeconds()
+    }));
+  }
+
+  if(urlReq.pathname === '/api/unixtime') {
+    res.end(JSON.stringify({
+      unixtime: date.getTime()
+    }))
+  }
+
+})
+server.listen(process.argv[2]);
